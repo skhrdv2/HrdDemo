fc4c59672c8dee84b23de486891b6bbded424407
JACKY HOKK <42473818+skhrdv2@users.noreply.github.com>
2018-09-17 12:31:58 +0700

    up date host

--- lib/host.php
+++ lib/host.php
@@ -1,16 +1,8 @@
 <?php
 date_default_timezone_set("Asia/Bangkok"); 
-<<<<<<< HEAD
-
 define("host", "202.80.228.36");
 define("username", "skhrd");
 define("password", "12345678");
-
-=======
-define("host", "202.80.228.36");
-define("username", "skhrd");
-define("password", "12345678");
->>>>>>> c0ed26c084535283b43e915850649b93daea9419
 define("db", "hrd");
 $db_report = mysqli_connect(host, username, password,db); 
 $query_report = $db_report ->query("SET NAMES UTF8");
