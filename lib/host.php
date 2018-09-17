46d33b2b788f852461ea86077075ce7bbb3c6065
DESKTOP-HAQSS2E\yeawnote <yeawyow@gmail.com>
2018-09-17 11:29:52 +0700

    666

--- lib/host.php
+++ lib/host.php
@@ -1,15 +1,8 @@
 <?php
 date_default_timezone_set("Asia/Bangkok"); 
-<<<<<<< HEAD
-define("host", "localhost");
-define("username", "hrd");
-define("password", "hrd");
-=======
 define("host", "202.80.228.36");
 define("username", "skhrd");
 define("password", "12345678");
-
->>>>>>> ae1a4827a576a240ded542bcdccc9a0bb27bbcfc
 define("db", "hrd");
 $db_report = mysqli_connect(host, username, password,db); 
 $query_report = $db_report ->query("SET NAMES UTF8");
